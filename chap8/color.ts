5ce147f835b106b97327120cafcc61589aab946f
Mike Tahani <me@miketahani.com>
2021-11-26 15:34:22 -0800

    add note about gamma

--- chap8/color.ts
+++ chap8/color.ts
@@ -6,7 +6,7 @@ export function write_color(
   pixel_color: Vec3,
   samples_per_pixel: number
 ): void {
-  // Divide the color by the number of samples
+  // Divide the color by the number of samples and gamma-correct for gamma=2.0
   const scale = 1.0 / samples_per_pixel
   const r = 255 * clamp(Math.sqrt(pixel_color.x() * scale), 0.0, 0.999)
   const g = 255 * clamp(Math.sqrt(pixel_color.y() * scale), 0.0, 0.999)
