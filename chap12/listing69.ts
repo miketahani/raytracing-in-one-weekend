3d0d15dc81d97b4e0cd32ec695668d7db9554dbd
Mike Tahani <me@miketahani.com>
2021-11-28 02:28:55 -0800

    formatting

--- chap12/listing69.ts
+++ chap12/listing69.ts
@@ -55,11 +55,11 @@ function main () {
   const material_left = dielectric(1.5)
   const material_right = metal(color(0.8, 0.6, 0.2), 0.0)
 
-  world.add(sphere(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-  world.add(sphere(point3( 0.0, 0.0, -1.0), 0.5, material_center));
-  world.add(sphere(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-  world.add(sphere(point3(-1.0, 0.0, -1.0), -0.45, material_left));
-  world.add(sphere(point3( 1.0, 0.0, -1.0), 0.5, material_right));
+  world.add(sphere(point3( 0.0, -100.5, -1.0), 100.0, material_ground))
+  world.add(sphere(point3( 0.0, 0.0, -1.0), 0.5, material_center))
+  world.add(sphere(point3(-1.0, 0.0, -1.0), 0.5, material_left))
+  world.add(sphere(point3(-1.0, 0.0, -1.0), -0.45, material_left))
+  world.add(sphere(point3( 1.0, 0.0, -1.0), 0.5, material_right))
 
   // Camera
   const lookfrom = point3(3, 3, 2)
